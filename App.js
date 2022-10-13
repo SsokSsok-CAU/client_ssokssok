63f5793a0cdefd056ba96f05c0ea27da4ab4a127
SeongOh Park <qkrtjddh1212@naver.com>
2022-10-14 00:12:58 +0900

    temp: image picker

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  const [image, setImage] = useState(null);
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result)
+    
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+    //image에 imageURL가 들어가있음
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       <StatusBar style="auto" />
     </View>
   );
