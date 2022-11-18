7c1d9b5c85477ca65c3c923bde0f17dccc9ffb6f
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-18 11:36:25 +0900

    fix(ml): 안돌아가는 머신러닝 서버 수정

--- core/configs/axios.js
+++ core/configs/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'https://mvp1---serverssokssok-m52f5s4c4q-du.a.run.app',
+  baseURL: 'https://ssokssokmvp-m52f5s4c4q-du.a.run.app/',
 });
